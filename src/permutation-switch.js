730e85b9a6cd49db7bee224bdc9f6e2958c0b66a
Atticus White <contact@atticuswhite.com>
2015-05-28 15:08:22 -0400

    permutation switch directive functionality

--- src/permutation-switch.js
+++ src/permutation-switch.js
@@ -1,34 +1,66 @@
-'use strict';
+(function () {
+  'use strict';
+  var MODULE_NAME = 'ngPermutationSwitch';
 
-var MODULE_NAME = 'ngPermutationSwitch';
+  function PermutationSwitchDirective ($parse) {
+    return {
+      restrict: 'AE',
+      require: 'permutationSwitch',
+      priority: 1200,
+      controller: ['$scope', function NgPermutationSwitchCtrl () {
+        this.permutations = [];
 
-function PermutationSwitchDirective () {
-  return {
-    restrict: 'AE',
-    controller: [function NgPermutationSwitchCtrl () {
-      this.permutations = [];
-    }],
-    link: function (scope, element, attrs) {
-      var watchExpression = attrs.permutationSwitch || attrs.on;
+        this.addPermutation = function (permutation) {
+          this.permutations.push(permutation);
+        };
+      }],
+      link: function (scope, element, attrs, permutationSwitchController) {
+        var watchExpression = attrs.permutationSwitch || attrs.on,
+            selectedPermutations = [];
 
-      scope.$watch(watchExpression, function permutationSwitchWatchAction (value) {
+        scope.$watch(watchExpression, function permutationSwitchWatchAction (value) {
+          var selectedPermutation;
+          while (selectedPermutations.length > 0) {
+            selectedPermutation = selectedPermutations.pop();
+            selectedPermutation.scope.$destroy();
+            selectedPermutation.element.remove();
+          }
 
-      });
-    }
-  };
-}
+          angular.forEach(permutationSwitchController.permutations, function (selectedTransclude) {
+            var permutation = $parse(selectedTransclude.expression)(scope);
+            for (var key in permutation) {
+              if (value[key] !== permutation[key]) {
+                return;
+              }
+            }
 
-function PermutationSwitchWhenDirective () {
-  return {
-    restrict: 'AE',
-    require: '^permutationSwitch',
-    transclude: 'element',
-    link: function (scope, element, attrs, ctrl, $transclude) {
-      ctrl.addPermutation({element: element, transclude: $transclude, expression: attrs.permutationSwitchWhen});
-    }
-  };
-}
+            selectedTransclude.transclude(function (permutationElement, selectedScope) {
+              selectedPermutations.push({
+                element: permutationElement,
+                scope: selectedScope
+              });
+              element.append(permutationElement);
+            });
+          });
+        });
+      }
+    };
+  }
 
-angular.module('NgPermutationSwitch', [])
-.directive('permutationSwitch', [PermutationSwitchWhenDirective])
-.directive('permtuationSwitchWhen', [PermutationSwitchWhenDirective]);
+  function PermutationSwitchWhenDirective () {
+    return {
+      restrict: 'AE',
+      require: '^permutationSwitch',
+      transclude: 'element',
+      priority: 1200,
+      link: function (scope, element, attrs, ctrl, $transclude) {
+        ctrl.addPermutation({element: element, transclude: $transclude, expression: attrs.permutationSwitchWhen});
+      }
+    };
+  }
+
+  angular.module('ngPermutationSwitch', [])
+  .directive('permutationSwitch', ['$parse', PermutationSwitchDirective])
+  .directive('permutationSwitchWhen', [PermutationSwitchWhenDirective]);
+
+}).call(this);
