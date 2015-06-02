9b31f46f0d6f24a6bb61967a0f981434add9edcc
Atticus White <contact@atticuswhite.com>
2015-06-02 15:01:28 -0400

    code style and linters

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,16 @@
 var gulp = require('gulp'),
+    jscs = require('gulp-jscs'),
+    jshint = require('gulp-jshint'),
     karma = require('karma').server;
+
+gulp.task('lint', function () {
+  gulp.src([
+    'src/**/*.js',
+    'test/**/*.js'
+  ])
+  .pipe(jshint())
+  .pipe(jscs());
+});
 
 gulp.task('test', function (done) {
   karma.start({
@@ -7,3 +18,5 @@ gulp.task('test', function (done) {
     singleRun: true
   }, done);
 });
+
+gulp.task('default', ['lint', 'test']);
