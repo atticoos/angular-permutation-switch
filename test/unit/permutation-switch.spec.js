9320f899fc4d997b12014258a1dea25788621960
Atticus White <contact@atticuswhite.com>
2015-06-02 14:36:32 -0400

    basic permutation tests

--- test/unit/permutation-switch.spec.js
+++ test/unit/permutation-switch.spec.js
@@ -38,4 +38,44 @@ describe('permutation-switch', function () {
       expect(getPermutations().eq(1).length).to.equal(0);
     });
   });
+
+  describe ('basic permutations', function () {
+    beforeEach(inject(function ($compile, $rootScope) {
+      scope = $rootScope.$new();
+      scope.one = true;
+      scope.two = true;
+      element = $compile([
+        '<div permutation-switch="{foo: one, bar: two}">',
+        '  <div permutation-switch-when="{foo: true, bar: true}">First</div>',
+        '  <div permutation-switch-when="{foo: true, bar: false}">Second</div>',
+        '  <div permutation-switch-when="{foo: false, bar: true}">Third</div>',
+        '  <div permutation-switch-when="{foo: false, bar: false}">Fourth</div>',
+        '</div>'
+      ].join('\n'))(scope);
+      scope.$apply();
+    }));
+    it ('should show the element if the case is satisfied and hide the others', function () {
+      expect(getPermutations().eq(0).text()).to.equal('First');
+      expect(getPermutations().eq(1).length).to.equal(0);
+      expect(getPermutations().eq(2).length).to.equal(0);
+      expect(getPermutations().eq(3).length).to.equal(0);
+    });
+    it ('should reflect the proper case when the premise changes', function () {
+      expect(getPermutations().eq(0).text()).to.equal('First');
+      expect(getPermutations().eq(1).length).to.equal(0);
+      scope.two = false;
+      scope.$apply();
+      expect(getPermutations().eq(0).text()).to.equal('Second');
+      expect(getPermutations().eq(1).length).to.equal(0);
+      scope.one = false;
+      scope.two = true;
+      scope.$apply();
+      expect(getPermutations().eq(0).text()).to.equal('Third');
+      expect(getPermutations().eq(1).length).to.equal(0);
+      scope.two = false;
+      scope.$apply();
+      expect(getPermutations().eq(0).text()).to.equal('Fourth');
+      expect(getPermutations().eq(1).length).to.equal(0);
+    });
+  });
 });
